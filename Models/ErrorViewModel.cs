56e4cc0edc4d90d86881cfa89b6a13a91a12e95a
Dakota Schramm <william.schramm11@gmail.com>
2026-03-22 14:33:46 -0500

    Init project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace youtube_notes.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
