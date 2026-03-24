d3cc6dbe2fb6286bf047f8f688135f81619415ed
Dakota Schramm <william.schramm11@gmail.com>
2026-03-24 12:18:51 -0500

    Add migration for creating Note model

---
+++ Migrations/20260324171754_CreateNote.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace youtube_notes.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateNote : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Note",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Comment = table.Column<string>(type: "TEXT", nullable: false),
+                    YoutubeId = table.Column<string>(type: "TEXT", nullable: false),
+                    TimeAt = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Note", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Note");
+        }
+    }
+}
