ce199a68ce0d7e1ee81cbe11d17c2277ec785896
Dakota Schramm <william.schramm11@gmail.com>
2026-03-24 11:35:54 -0500

    Edit Search controller to create embed url for video player
    - in future, we should add better error handling and handle urls that arent in the correct format

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -6,7 +6,23 @@ public class SearchController : Controller
 {
     public IActionResult Index(string? url)
     {
-        ViewData["VideoUrl"] = url;
+        ViewData["VideoUrl"] = ConvertToEmbedUrl(url);
         return View();
+    }
+
+    private static string? ConvertToEmbedUrl(string? url)
+    {
+        if (string.IsNullOrEmpty(url))
+            return null;
+
+        if (Uri.TryCreate(url, UriKind.Absolute, out var uri))
+        {
+            var query = System.Web.HttpUtility.ParseQueryString(uri.Query);
+            var videoId = query["v"];
+            if (!string.IsNullOrEmpty(videoId))
+                return $"https://www.youtube.com/embed/{videoId}";
+        }
+
+        return null;
     }
 }
